fad33f39b7052249978a493fe5eed457ab5894f6
Eduardo <joselincango73@gmail.com>
2024-06-20 09:54:30 -0500

    Primer comando interfaz

--- Clase20-06-2024/src/Main.java
+++ Clase20-06-2024/src/Main.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
-        }
+        //Entrada de usuario de los dialogos
+        String primernumero = JOptionPane.showInputDialog("Introduzca el prmer numero: ");
+        String segundonumero = JOptionPane.showInputDialog("Introduzca el segundo numero: ");
+
+        //Convierte entradas String en valores int
+
+        int numero1 = Integer.parseInt(primernumero);
+        int numero2 = Integer.parseInt(segundonumero);
+        int multiplica = numero1 * numero2;
+
+        //Muestra de resultados en un dialogo
+        JOptionPane.showMessageDialog(null, "La smultiplicacion es: " + multiplica, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE );
     }
-//..
+}
+// + - * /
